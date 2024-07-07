4de4f391783535fac83bbf5c7acee8818ce7b0fa
Pavel Bandarenka <bondarenkop76@gmail.com>
2024-07-07 15:27:34 +0300

    changing the format for specifying the path to read files

--- src/main/java/ru/clevertec/check/CheckRunner.java
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -13,8 +13,8 @@ import java.util.Locale;
 import java.util.Map;
 
 public class CheckRunner {
-    private final static String PRODUCTS_FILE_PATH = "src/main/resources/products.csv";
-    private final static String DISCOUNT_CARDS_FILE_PATH = "src/main/resources/discountCards.csv";
+    private final static String PRODUCTS_FILE_PATH = "./src/main/resources/products.csv";
+    private final static String DISCOUNT_CARDS_FILE_PATH = "./src/main/resources/discountCards.csv";
     private final static String RESULT_FILE_PATH = "result.csv";
 
     public static void main(String[] args) {
