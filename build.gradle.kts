3286c33d5ca11e3cb0aa93e39b2bb522503dc358
Pavel Bandarenka <bondarenkop76@gmail.com>
2024-07-09 19:34:40 +0300

    add some build properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "ru.clevertec"
@@ -9,10 +10,28 @@ repositories {
     mavenCentral()
 }
 
+
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation ("com.h2database:h2:2.2.220")
     implementation("org.postgresql:postgresql:42.7.2")
+}
+
+application {
+    mainClass.set("ru.clevertec.CheckRunner")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "ru.clevertec.check.CheckRunner"
+    }
+    archiveBaseName.set("clevertec-check")
+    archiveVersion.set("")
+    from({
+        configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+    })
 }
 
 tasks.test {
