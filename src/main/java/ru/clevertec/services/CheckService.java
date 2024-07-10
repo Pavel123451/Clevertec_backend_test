0a4ab4a424e1eb23de45921fb528dada4c0f50de
Pavel Bandarenka <bondarenkop76@gmail.com>
2024-07-10 23:16:39 +0300

    refactor CheckService.java

--- src/main/java/ru/clevertec/services/CheckService.java
+++ src/main/java/ru/clevertec/services/CheckService.java
@@ -1,18 +1,20 @@
 package ru.clevertec.services;
 
 
+import ru.clevertec.dao.impl.ProductDao;
 import ru.clevertec.exceptions.BadRequestException;
 import ru.clevertec.exceptions.NotEnoughMoneyException;
 import ru.clevertec.models.DiscountCard;
 import ru.clevertec.models.Product;
+import static ru.clevertec.servlets.CheckServlet.CheckRequest;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class CheckService {
-    private final static int PRODUCT_ARG_PARTS_COUNT = 2;
     private final static int QTY_FOR_WHOLESALE = 5;
     private List<Product> products;
     private DiscountCard discountCard;
@@ -24,33 +26,129 @@ public class CheckService {
         this.balance = balance;
     }
 
-    private Map<Integer, Integer> parseProductQuantities(String productArgs)
-            throws BadRequestException {
-        Map<Integer, Integer> productQuantities = new HashMap<>();
-        String[] args = productArgs.split(" ");
+    public String generateCheck(
+            List<CheckRequest.ProductQuantity> productQuantities,
+            ProductDao productDao)
+            throws BadRequestException, NotEnoughMoneyException, SQLException {
+        validateProducts(productQuantities);
 
-        for (String arg : args) {
-            String[] parts = arg.split("-");
-            if (parts.length != PRODUCT_ARG_PARTS_COUNT) {
-                throw new BadRequestException("Invalid product argument format: " + arg);
-            }
-            int id = 0;
-            int quantity = 0;
-            try {
-                id = Integer.parseInt(parts[0]);
-                quantity = Integer.parseInt(parts[1]);
-            } catch (NumberFormatException e) {
-                throw new BadRequestException("Invalid product argument format: " + arg);
-            }
-            if (quantity <= 0) {
-                throw new BadRequestException("Invalid quantity: " + quantity);
-            }
-            productQuantities.merge(id, quantity, Integer::sum);
+        List<ProductInfo> productInfoList = calculateProductInfo(productQuantities);
+        BigDecimal totalCost = calculateTotalCost(productInfoList);
+        if (balance < totalCost.doubleValue()) {
+            throw new NotEnoughMoneyException("Not enough money on the debit card");
         }
-        return productQuantities;
+
+        StringBuilder csvContent = generateCheckContent(productInfoList, totalCost);
+
+        updateProductQuantities(productQuantities, productDao);
+
+        return csvContent.toString();
     }
 
-    private BigDecimal calculateDiscount(Product product, int quantity, StringBuilder discountDetails) {
+    private void validateProducts(List<CheckRequest.ProductQuantity> productQuantities)
+            throws BadRequestException {
+        for (CheckRequest.ProductQuantity pq : productQuantities) {
+            Product product = getProductById(pq.getId());
+            if (pq.getQuantity() > product.getQuantityInStock()) {
+                throw new BadRequestException("Not enough stock for product id: " + pq.getId());
+            }
+        }
+    }
+
+    private Product getProductById(int id) throws BadRequestException {
+        return products.stream()
+                .filter(p -> p.getId() == id)
+                .findFirst()
+                .orElseThrow(() -> new BadRequestException("Product with id " + id + " not found"));
+    }
+
+    private List<ProductInfo> calculateProductInfo(
+            List<CheckRequest.ProductQuantity> productQuantities
+    ) throws BadRequestException {
+        List<ProductInfo> productInfoList = new ArrayList<>();
+        for (CheckRequest.ProductQuantity pq : productQuantities) {
+            Product product = getProductById(pq.getId());
+            int quantity = pq.getQuantity();
+
+            BigDecimal productPrice = BigDecimal.valueOf(product.getPrice());
+            BigDecimal productTotalCost = productPrice.multiply(BigDecimal.valueOf(quantity));
+            StringBuilder discountDetails = new StringBuilder();
+            BigDecimal discount = calculateDiscount(product, quantity, discountDetails);
+
+            BigDecimal costAfterDiscount = productTotalCost.subtract(discount);
+
+            productInfoList.add(new ProductInfo(
+                    product,
+                    quantity,
+                    productPrice,
+                    productTotalCost,
+                    discount,
+                    costAfterDiscount
+            ));
+        }
+        return productInfoList;
+    }
+
+    private BigDecimal calculateTotalCost(List<ProductInfo> productInfoList) {
+        return productInfoList.stream()
+                .map(ProductInfo::costAfterDiscount)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private StringBuilder generateCheckContent(List<ProductInfo> productInfoList, BigDecimal totalCost) {
+        StringBuilder csvContent = new StringBuilder();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
+        Date now = new Date();
+
+        csvContent.append("Date;Time\n")
+                .append(dateFormat.format(now)).append(";")
+                .append(timeFormat.format(now)).append("\n\n")
+                .append("QTY;DESCRIPTION;PRICE;DISCOUNT;TOTAL\n");
+
+        BigDecimal totalDiscount = BigDecimal.ZERO;
+        BigDecimal totalWithoutDiscount = BigDecimal.ZERO;
+
+        for (ProductInfo productInfo : productInfoList) {
+            csvContent.append(String.format("%d;%s;%.2f$;%.2f$;%.2f$\n",
+                    productInfo.quantity(),
+                    productInfo.product().getDescription(),
+                    productInfo.productPrice(),
+                    productInfo.discount(),
+                    productInfo.productTotalCost()));
+
+            totalDiscount = totalDiscount.add(productInfo.discount());
+            totalWithoutDiscount = totalWithoutDiscount.add(productInfo.productTotalCost());
+        }
+
+        if (discountCard != null) {
+            csvContent.append("\nDISCOUNT CARD;DISCOUNT PERCENTAGE\n")
+                    .append(discountCard.getNumber()).append(";")
+                    .append(discountCard.getDiscountPercentage()).append("%\n");
+        }
+
+        csvContent.append("\nTOTAL PRICE;TOTAL DISCOUNT;TOTAL WITH DISCOUNT\n")
+                .append(String.format("%.2f$;%.2f$;%.2f$\n",
+                        totalWithoutDiscount, totalDiscount, totalCost));
+
+        return csvContent;
+    }
+
+    private void updateProductQuantities(
+            List<CheckRequest.ProductQuantity> productQuantities,
+            ProductDao productDao
+    ) throws SQLException, BadRequestException {
+        for (CheckRequest.ProductQuantity pq : productQuantities) {
+            Product product = getProductById(pq.getId());
+            product.setQuantityInStock(product.getQuantityInStock() - pq.getQuantity());
+            productDao.updateProductQuantity(product.getId(), product.getQuantityInStock());
+        }
+    }
+
+    private BigDecimal calculateDiscount(
+            Product product,
+            int quantity,
+            StringBuilder discountDetails) {
         BigDecimal discount = BigDecimal.ZERO;
 
         if (product.isWholesaleProduct() && quantity >= QTY_FOR_WHOLESALE) {
@@ -69,85 +167,9 @@ public class CheckService {
         return discount.setScale(2, RoundingMode.HALF_UP);
     }
 
-    public String generateCheck(Map<String, String> params)
-            throws BadRequestException, NotEnoughMoneyException {
-        String productArgs = params.get("productArgs");
-        Map<Integer, Integer> productQuantities = parseProductQuantities(productArgs);
-
-        StringBuilder csvContent = new StringBuilder();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
-        Date now = new Date();
-
-        csvContent.append("Date;Time\n");
-        csvContent.append(dateFormat.format(now)).append(";")
-                .append(timeFormat.format(now)).append("\n\n");
-        System.out.println("Date: " + dateFormat.format(now));
-        System.out.println("Time: " + timeFormat.format(now));
-        System.out.println();
-
-        csvContent.append("QTY;DESCRIPTION;PRICE;DISCOUNT;TOTAL\n");
-        System.out.println("QTY\tDESCRIPTION\tPRICE\tDISCOUNT\tTOTAL");
-
-        BigDecimal totalCost = BigDecimal.ZERO;
-        BigDecimal totalDiscount = BigDecimal.ZERO;
-        BigDecimal totalWithoutDiscount = BigDecimal.ZERO;
-
-        for (Map.Entry<Integer, Integer> entry : productQuantities.entrySet()) {
-            int id = entry.getKey();
-            int quantity = entry.getValue();
-
-            Product product = products.stream()
-                    .filter(p -> p.getId() == id)
-                    .findFirst()
-                    .orElseThrow(() -> new BadRequestException("Product with id " + id + " not found"));
-
-            if (quantity > product.getQuantityInStock()) {
-                throw new BadRequestException("Not enough stock for product id: " + id);
-            }
-
-            BigDecimal productPrice = BigDecimal.valueOf(product.getPrice());
-            BigDecimal productTotalCost = productPrice.multiply(BigDecimal.valueOf(quantity));
-            StringBuilder discountDetails = new StringBuilder();
-            BigDecimal discount = calculateDiscount(product, quantity, discountDetails);
-
-            BigDecimal costAfterDiscount = productTotalCost.subtract(discount);
-
-            csvContent.append(String.format("%d;%s;%.2f$;%.2f$;%.2f$\n",
-                    quantity, product.getDescription(),
-                    productPrice, discount, productTotalCost));
-            System.out.printf("%d\t%s\t%.2f$\t%.2f$\t%.2f$%n", quantity,
-                    product.getDescription(), productPrice, discount, productTotalCost);
-            if (discount.compareTo(BigDecimal.ZERO) > 0) {
-                System.out.println(discountDetails);
-            }
-
-            totalCost = totalCost.add(costAfterDiscount);
-            totalDiscount = totalDiscount.add(discount);
-            totalWithoutDiscount = totalWithoutDiscount.add(productTotalCost);
-        }
-
-        if (discountCard != null) {
-            csvContent.append("\nDISCOUNT CARD;DISCOUNT PERCENTAGE\n");
-            csvContent.append(discountCard.getNumber()).append(";")
-                    .append(discountCard.getDiscountPercentage()).append("%\n");
-            System.out.println();
-            System.out.println("DISCOUNT CARD: " + discountCard.getNumber());
-            System.out.println("DISCOUNT PERCENTAGE: " + discountCard.getDiscountPercentage() + "%");
-        }
-
-        if (balance < totalCost.doubleValue()) {
-            throw new NotEnoughMoneyException("Not enough money on the debit card");
-        }
-
-        csvContent.append("\nTOTAL PRICE;TOTAL DISCOUNT;TOTAL WITH DISCOUNT\n");
-        csvContent.append(String.format("%.2f$;%.2f$;%.2f$\n",
-                totalWithoutDiscount, totalDiscount, totalCost));
-        System.out.println();
-        System.out.println("TOTAL PRICE: " + totalWithoutDiscount + "$");
-        System.out.println("TOTAL DISCOUNT: " + totalDiscount + "$");
-        System.out.println("TOTAL WITH DISCOUNT: " + totalCost + "$");
-
-        return csvContent.toString();
+    private record ProductInfo(Product product, int quantity,
+                               BigDecimal productPrice,
+                               BigDecimal productTotalCost, BigDecimal discount,
+                               BigDecimal costAfterDiscount) {
     }
 }
